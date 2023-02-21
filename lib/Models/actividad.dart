9fad1c3e18aa72d1cd0e863429b08e248c17da11
Tomas <tomaspalaoro@elcampico.org>
2023-02-21 17:01:23 +0100

    mostrar listado actividades firebase

--- lib/Models/actividad.dart
+++ lib/Models/actividad.dart
@@ -2,4 +2,12 @@ import 'package:flutter_transylvania/Models/modelo.dart';
 
 class Actividad extends Modelo {
   Actividad({super.nombre, super.descripcion, super.imagen});
+
+  factory Actividad.fromJson(Map<String, dynamic> json) {
+    return Actividad(
+      nombre: json['nombre'],
+      descripcion: json['descripcion'],
+      imagen: json['imagen'],
+    );
+  }
 }
--- lib/Screens/actividades.dart
+++ lib/Screens/actividades.dart
@@ -2,24 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_transylvania/Models/actividad.dart';
 import 'package:flutter_transylvania/Components/listado.dart';
 
-class ActividadesScreen extends StatelessWidget {
-  List<Actividad> actividades = [
-    Actividad(
-        nombre: "Prueba 1",
-        descripcion: "Esto es una prueba",
-        imagen: "https://via.placeholder.com/150"),
-    Actividad(
-        nombre: "Prueba 2",
-        descripcion: "Esto es otra prueba",
-        imagen: "https://via.placeholder.com/150"),
-  ];
+import 'dart:convert'; //para trabajar con JSON
+import 'package:http/http.dart' as http; //import http
+
+final String ruta =
+    "https://fluttertransylvania-default-rtdb.firebaseio.com/Actividad.json";
+
+class ActividadesScreen extends StatefulWidget {
+  @override
+  State<ActividadesScreen> createState() => _ActividadesScreenState();
+}
+
+class _ActividadesScreenState extends State<ActividadesScreen> {
+  List<Actividad> actividades = [];
+
+  Future<dynamic> _getActividades() async {
+    try {
+      final url = Uri.parse(ruta);
+      final response = await http.get(url);
+      ;
+      if (response.statusCode == 200) {
+        List<dynamic> arrayJSON = jsonDecode(response.body);
+        for (var element in arrayJSON) {
+          actividades.add(Actividad.fromJson(element));
+        }
+      } else {
+        print('Petición fallida: ${response.statusCode}.');
+      }
+    } catch (e) {
+      print('Exception: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Actividades"),
       ),
-      body: listado(actividades),
+      body: FutureBuilder(
+        future: _getActividades(),
+        builder: (context, snapshot) => listado(actividades),
+      ),
     );
   }
 }
